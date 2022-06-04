1767d5845fc0fc3a12d03675d3a66ca14376a0b3
yuri-vashchenko <yuri.vashchenko@gmail.com>
2022-06-04 21:17:08 +1000

    update ansible.cfg and simplify pass.py

--- ansible/pass.py
+++ ansible/pass.py
@@ -3,15 +3,12 @@
 import sys
 import yaml
 
-vault_id = "test"
-if (len(sys.argv) > 1):
-    vault_id = sys.argv[1]
-
+# sys.stderr.write("Requested ansible password\n")
 secrets = "/home/yuri/.local/secret.yaml"
 
 with open(secrets, "r") as stream:
     try:
         data = yaml.safe_load(stream)
-        print(data[vault_id]["ansible_vault_password"])
+        print(data["ansible_vault_password"])
     except yaml.YAMLError as exc:
-        print(exc)
+        sys.stderr.write(exc)
